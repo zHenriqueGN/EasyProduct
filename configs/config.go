79ebbabc4e161b0506ec31502acc1efbe443c5ca
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-09-11 15:56:47 -0300

    feat: create config file

---
+++ configs/config.go
@@ -0,0 +1,35 @@
+package configs
+
+import (
+	"github.com/go-chi/jwtauth/v5"
+	"github.com/spf13/viper"
+)
+
+var cfg *conf
+
+type conf struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBHost        string `mapstructure:"DB_HOST"`
+	DBPort        string `mapstructure:"DB_PORT"`
+	DBUser        string `mapstructure:"DB_USER"`
+	DBPassword    string `mapstructure:"DB_PASSWORD"`
+	DBName        string `mapstructure:"DB_NAME"`
+	WebServerPort string `mapstructure:"WEB_SERVER_PORT"`
+	JWTSecret     string `mapstructure:"JWT_SECRET"`
+	JWTExperesIn  int    `mapstructure:"JWT_EXPIRESIN"`
+	TokenAuth     *jwtauth.JWTAuth
+}
+
+func LoadConfig() *conf {
+	viper.SetConfigFile(".env")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		panic(err)
+	}
+	cfg.TokenAuth = jwtauth.New("HS256", []byte(cfg.JWTSecret), nil)
+	return cfg
+}
