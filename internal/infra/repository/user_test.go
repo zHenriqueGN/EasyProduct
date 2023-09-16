6c217c415312c404ad37f48a73152fc53a16cd3d
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-09-15 21:03:14 -0300

    refactor: use mysql as sample database in user tests

--- internal/infra/repository/user_test.go
+++ internal/infra/repository/user_test.go
@@ -1,31 +1,16 @@
 package repository
 
 import (
-	"database/sql"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/zHenriqueGN/EasyProduct/internal/entity"
+	"github.com/zHenriqueGN/EasyProduct/internal/infra/database"
 )
 
-func createTestUsersTable(t *testing.T, DB *sql.DB) {
-	_, err := DB.Exec(`
-		CREATE TABLE users (
-			id TEXT PRIMARY KEY,
-			name TEXT NOT NULL,
-			email TEXT NOT NULL UNIQUE,
-			password TEXT NOT NULL
-		)`,
-	)
-	if err != nil {
-		t.Fatal(err)
-	}
-}
-
 func TestUserRepository_Create(t *testing.T) {
-	DB := NewSQLiteDB(t)
+	DB := database.ConnectToMySQLDB(testDBConn)
 	defer DB.Close()
-	createTestUsersTable(t, DB)
 	repository := NewUserRepository(DB)
 	user, err := entity.NewUser("John Doe", "john.doe@example.com", "123456")
 	if err != nil {
@@ -36,11 +21,10 @@ func TestUserRepository_Create(t *testing.T) {
 }
 
 func TestUserRepository_FindByEmail(t *testing.T) {
-	DB := NewSQLiteDB(t)
+	DB := database.ConnectToMySQLDB(testDBConn)
 	defer DB.Close()
-	createTestUsersTable(t, DB)
 	repository := NewUserRepository(DB)
-	user, err := entity.NewUser("John Doe", "john.doe@example.com", "123456")
+	user, err := entity.NewUser("Kevin Rue", "kevin.rue@example.com", "123456")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -48,7 +32,7 @@ func TestUserRepository_FindByEmail(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	userFound, err := repository.FindByEmail("john.doe@example.com")
+	userFound, err := repository.FindByEmail("kevin.rue@example.com")
 	assert.Nil(t, err)
 	assert.NotNil(t, userFound)
 	assert.Equal(t, user.ID, userFound.ID)
