ec460e5a5bd240b1b2c32e08cb8fec3f22deba15
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-09-19 17:47:09 -0300

    feat: create GetJWTInput DTO

--- internal/dto/dto.go
+++ internal/dto/dto.go
@@ -10,3 +10,7 @@ type CreateUserInput struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
+type GetJWTInput struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
