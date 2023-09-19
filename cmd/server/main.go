0deba8996a6764d01f9384cd92eaa910539f55a2
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-09-19 20:50:39 -0300

    feat: add jwt middleware over product routes

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/jwtauth/v5"
 	"github.com/zHenriqueGN/EasyProduct/configs"
 	"github.com/zHenriqueGN/EasyProduct/internal/infra/database"
 	"github.com/zHenriqueGN/EasyProduct/internal/infra/repository"
@@ -21,11 +22,15 @@ func main() {
 	userHandler := handlers.NewUserHandler(userRepository, cfg.TokenAuth, cfg.JWTExperesIn)
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Post("/products", productHandler.CreateProduct)
-	r.Get("/products", productHandler.GetProducts)
-	r.Get("/products/{id}", productHandler.GetProduct)
-	r.Put("/products/{id}", productHandler.UpdateProduct)
-	r.Delete("/products/{id}", productHandler.DeleteProduct)
+	r.Route("/products", func(r chi.Router) {
+		r.Use(jwtauth.Verifier(cfg.TokenAuth))
+		r.Use(jwtauth.Authenticator)
+		r.Post("/", productHandler.CreateProduct)
+		r.Get("/", productHandler.GetProducts)
+		r.Get("/{id}", productHandler.GetProduct)
+		r.Put("/{id}", productHandler.UpdateProduct)
+		r.Delete("/{id}", productHandler.DeleteProduct)
+	})
 	r.Post("/users", userHandler.CreateUser)
 	r.Post("/users/getjwt", userHandler.GetJWT)
 	http.ListenAndServe(":8000", r)
