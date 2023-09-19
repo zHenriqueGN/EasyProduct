65bd6e13f8c601c858d47ae9afe251aa4ad20be2
Henrique Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-09-18 22:11:44 -0300

    fix: fix the products FindAll

--- internal/infra/repository/interfaces.go
+++ internal/infra/repository/interfaces.go
@@ -9,7 +9,7 @@ type UserInterface interface {
 
 type ProductInterface interface {
 	Create(product *entity.Product) error
-	FindAll(page, limit int, sort string) ([]entity.Product, error)
+	FindAll(page, limit int) ([]entity.Product, error)
 	FindByID(id string) (*entity.Product, error)
 	Update(product *entity.Product) error
 	Delete(id string) error
