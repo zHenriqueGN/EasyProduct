9176a0cfec1a02686bcdcd1e47acc80e80c956c5
Kennedy Gomes Nunes <henriquegomesnunes19@gmail.com>
2023-09-22 12:55:20 -0300

    feat: add error message in CreateUser messages

--- internal/infra/webserver/handlers/user.go
+++ internal/infra/webserver/handlers/user.go
@@ -24,16 +24,19 @@ func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&userInput)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(Error{Message: err.Error()})
 		return
 	}
 	user, err := entity.NewUser(userInput.Name, userInput.Email, userInput.Password)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(Error{Message: err.Error()})
 		return
 	}
 	err = h.UserRepository.Create(user)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(Error{Message: err.Error()})
 		return
 	}
 	w.WriteHeader(http.StatusCreated)
@@ -44,15 +47,18 @@ func (h *UserHandler) GetJWT(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&getJWTInput)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(Error{Message: err.Error()})
 		return
 	}
 	user, err := h.UserRepository.FindByEmail(getJWTInput.Email)
 	if err != nil {
 		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(Error{Message: err.Error()})
 		return
 	}
 	if !user.ValidatePassword(getJWTInput.Password) {
 		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(Error{Message: err.Error()})
 		return
 	}
 	JWT := r.Context().Value("jwt").(*jwtauth.JWTAuth)
@@ -63,6 +69,7 @@ func (h *UserHandler) GetJWT(w http.ResponseWriter, r *http.Request) {
 	})
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(Error{Message: err.Error()})
 		return
 	}
 	accessToken := map[string]string{
